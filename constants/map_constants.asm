5bfc71e71c1614098d5c18c0ecd4f990865ec737
Zem <82421333+Zemaille@users.noreply.github.com>
2025-08-21 13:18:05 -0400

    Safari Update

--- constants/map_constants.asm
+++ constants/map_constants.asm
@@ -154,7 +154,6 @@ ENDM
 	map_const UNDERGROUND_PATH,                             3, 14 ; 86
 	map_const ROCK_TUNNEL_1F,                              15, 18 ; 87
 	map_const ROCK_TUNNEL_B1F,                             15, 18 ; 88
-	map_const SAFARI_ZONE_FUCHSIA_GATE_BETA,                5,  4 ; 89
 	map_const VICTORY_ROAD,                                10, 36 ; 91
 	map_const THUNDER_PEAK_1F,                             15,  9 ; 92
 	map_const THUNDER_PEAK_OUTSIDE,                        20,  9 ; 93
