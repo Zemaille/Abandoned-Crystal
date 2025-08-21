83fe1e005b92190bbf701bf161796652e6f265af
Zem <82421333+Zemaille@users.noreply.github.com>
2025-08-21 17:46:52 -0400

    Champ Club .asms finished for now

--- maps/PokeChampionClubInside.asm
+++ maps/PokeChampionClubInside.asm
@@ -1,9 +1,30 @@
 	object_const_def
+	const CHAMPCLUB_RECEPTIONIST
+	const CHAMPCLUB_RECEPTIONIST2
 
 PokeChampionClubInside_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+PokeChampionClubWelcomeScript:
+	faceplayer
+	jumptext PokeChampionClubWelcomeText
+	
+PokeChampionClubWelcomeText:
+	text "Welcome to the"
+	line "Pokémon Champion"
+	cont "Club!"
+	done
+
+PokeChampionClubStaffScript:
+	jumptext PokeChampionClubStaffText
+
+PokeChampionClubStaffText:
+	text "Sorry! League"
+	line "Staff only past"
+	cont "this point."
+	done
 
 PokeChampionClubInside_MapEvents:
 	db 0, 0 ; filler
@@ -17,4 +38,7 @@ PokeChampionClubInside_MapEvents:
 	def_bg_events
 
 	def_object_events
+	object_event  5, 10, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_RIGHT, 0, 0, -1, -1, PAL_NPC_GREEN, OBJECTTYPE_SCRIPT, 0, PokeChampionClubWelcomeScript, -1
+	object_event 15,  2, SPRITE_RECEPTIONIST, SPRITEMOVEDATA_STANDING_LEFT, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, PokeChampionClubStaffScript, -1
+
 	
--- maps/PokeChampionClubOutside.asm
+++ maps/PokeChampionClubOutside.asm
@@ -1,9 +1,19 @@
 	object_const_def
+	const GYM_GUIDE
 
 PokeChampionClubOutside_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+
+PokeChampClubOutsideBlockedScript:
+	faceplayer
+	jumptext PokeChampClubOutsideBlocked
+
+PokeChampClubOutsideBlocked:
+	text "Come back when"
+	line "you're the Champ!"
+	done
 
 PokeChampionClubOutside_MapEvents:
 	db 0, 0 ; filler
@@ -16,4 +26,4 @@ PokeChampionClubOutside_MapEvents:
 	def_bg_events
 
 	def_object_events
-
+	object_event  8, 10, SPRITE_GYM_GUIDE, SPRITEMOVEDATA_STANDING_DOWN, 0, 0, -1, -1, PAL_NPC_RED, OBJECTTYPE_SCRIPT, 0, PokeChampClubOutsideBlockedScript, EVENT_BEAT_ELITE_FOUR
