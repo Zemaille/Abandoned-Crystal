88b9d9483c0e9b0d22dc8463930404c3b0962b39
Zem <82421333+Zemaille@users.noreply.github.com>
2026-01-05 21:24:16 -0500

    Battle Tower & Saltmarsh Maps updated

--- data/tilesets/battle_tower_outside_collision.asm
+++ data/tilesets/battle_tower_outside_collision.asm
@@ -126,3 +126,9 @@
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 7d
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 7e
 	tilecoll FLOOR, FLOOR, FLOOR, FLOOR ; 7f
+	tilecoll WALL, WALL, WALL, WALL ; 80
+	tilecoll WALL, WALL, WALL, WALL ; 81
+	tilecoll WALL, WALL, WALL, WALL ; 82
+	tilecoll WALL, FLOOR, WALL, WALL ; 83
+	tilecoll WALL, WALL, WALL, WALL ; 84
+	tilecoll WALL, WALL, FLOOR, FLOOR ; 85
